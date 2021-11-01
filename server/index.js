65cdeeb4c3e5fd4ee1673ab628b8ed099f940bc8
Vladyslav Shchetyna <vshchetyna@lenovo.com>
2021-11-01 12:17:01 +0200

    Deleted try catch block

--- server/index.js
+++ server/index.js
@@ -2,15 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const PORT = 3000;
+const PORT = 5000;
 const app = express();
 
 const start = async () => {
-  try {
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 start();
