21cf08afc10a79e5907dd4a7fccdfeb272872097
Vladyslav Shchetyna <vshchetyna@lenovo.com>
2021-11-01 16:15:43 +0200

    Server setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const PORT = 2000;
+const PORT = 5000;
 const app = express();
 
 const start = async () => {
+  try {
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
